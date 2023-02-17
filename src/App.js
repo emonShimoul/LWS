0a28d5d0ea26574b8e4bb36acdbd56d0c25096d1
emonShimoul <emonshimoul@gmail.com>
2023-02-18 03:23:45 +0600

    counter app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import './App.css';
+import Counter from "./components/Counter";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <h2 className='text-3xl'>React Redux</h2>
+    <div className="w-screen h-screen p-10 bg-gray-100 text-slate-700">
+      <h1 className="max-w-md mx-auto text-center text-2xl font-bold">
+        Simple Counter Application
+      </h1>
+
+      <div className="max-w-md mx-auto mt-10 space-y-5">
+        <Counter />
+        <Counter />
+        <Counter />
+      </div>
     </div>
   );
 }
-
-export default App;
