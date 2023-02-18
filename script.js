0774714973b65b7bedf4476723f171817741055d
emonShimoul <emonshimoul@gmail.com>
2023-02-18 15:28:10 +0600

    counter app using redux completed

--- script.js
+++ script.js
@@ -1,19 +1,52 @@
 // select dom elements
-const counterEl = document.getElementById("counter");
 const incrementEl = document.getElementById("increment");
 const decrementEl = document.getElementById("decrement");
+const counterEl = document.getElementById("counter");
 
 // initial state
-let count = 0;
-let count2 = 0;
+const initialState = {
+    value: 0,
+}
+
+// reducer function
+const counterReducer = (state = initialState, action) => {
+    if (action.type === "increment") {
+        return {
+            ...state,
+            value: state.value + 1,
+        };
+    } else if (action.type === "decrement") {
+        return {
+            ...state,
+            value: state.value - 1,
+        };
+    } else {
+        return state;
+    }
+}
+
+// create store 
+const store = Redux.createStore(counterReducer);
+
+const render = () => {
+    const state = store.getState();
+    counterEl.innerText = state.value.toString();
+}
+
+// update UI initially
+render();
+
+store.subscribe(render);
 
 // event listeners
 incrementEl.addEventListener("click", () => {
-    count++;
-    counterEl.innerText = count;
+    store.dispatch({
+        type: "increment"
+    });
 });
 
 decrementEl.addEventListener("click", () => {
-    count--;
-    counterEl.innerText = count;
+    store.dispatch({
+        type: "decrement"
+    });
 });
