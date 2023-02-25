4621d18b0781ef2cee4a29f3f12d4f319a4ca41d
emonShimoul <emonshimoul@gmail.com>
2023-02-26 02:19:00 +0600

    update todo into the node server using redux-thunk

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import cancelImage from '../assets/images/cancel.png';
-import { toggled, colorSelected, deleted } from '../redux/todos/actions';
+import { deleted } from '../redux/todos/actions';
+import updateStatus from '../redux/todos/thunk/updateStatus';
+import updateColor from '../redux/todos/thunk/updateColor';
 
 const Todo = ({ todo }) => {
     const dispatch = useDispatch();
@@ -9,11 +11,11 @@ const Todo = ({ todo }) => {
     const { text, id, completed, color } = todo;
 
     const handleStatusChange = (todoId) => {
-        dispatch(toggled(todoId));
+        dispatch(updateStatus(todoId, completed));
     }
 
     const handleColorChange = (todoId, color) => {
-        dispatch(colorSelected(todoId, color));
+        dispatch(updateColor(todoId, color));
     }
 
     const handleDelete = (todoId) => {
@@ -25,7 +27,7 @@ const Todo = ({ todo }) => {
             className="flex justify-start items-center p-2 hover:bg-gray-100 hover:transition-all space-x-4 border-b border-gray-400/20 last:border-0"
         >
             <div
-                className={`rounded-full bg-white border-2 border-gray-400 w-5 h-5 flex flex-shrink-0 justify-center items-center mr-2 ${completed && "border-green-500 focus-within:border-green-500"}`}
+                className={`relative rounded-full bg-white border-2 border-gray-400 w-5 h-5 flex flex-shrink-0 justify-center items-center mr-2 ${completed && "border-green-500 focus-within:border-green-500"}`}
             >
                 <input
                     type="checkbox"
